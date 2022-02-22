85667c23143a413774b8c5ee322b9ff4b88c3448
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-02-22 00:54:06 -0500

    commit tree 570855722c8127c53d432b68d617b3c9fa6be1e4

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5]
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce(function(accumulator, element) {
+    return element += accumulator
+})
+// [array].reduce(newFunction(accumulator, element){return element + accumulator}, 0)
+
+// array selection, reduce action, newFunction creation, (accumulator/element parameters), {return computation each iteration}, beginning at index val)
